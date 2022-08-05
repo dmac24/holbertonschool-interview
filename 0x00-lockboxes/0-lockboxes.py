2c5926d5ea8f577d99725b05fa9d692bbd4b1ab0
dmac24 <dinitayala@gmail.com>
2022-08-04 22:33:04 -0500

    Modified Task -  Without answer yet

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" Lockboxes """
+
+from operator import truediv
+
+
+def canUnlockAll(boxes):
+""" method that determines if all the boxes can be opened"""
+total_boxes = len(boxes)
+
+if total_boxes <= 1:
+    return True
+return False
+
+
+
