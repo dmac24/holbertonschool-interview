43b777b58c16b746af4702910cac0dd963f11106
dmac24 <dinitayala@gmail.com>
2022-08-28 14:15:49 -0500

    Add Task

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Method that calculates the fewest number of operations"""
+
+import math
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operation = 0
+    while (n > 1):
+        for divisor in range(2, n + 1):
+            if n % divisor == 0:
+                operation += divisor
+                n = n // divisor
+    return operation
